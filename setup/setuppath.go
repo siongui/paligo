10b255fc82c6ff6b78b1ea26e0c686856fe3479e
siongui <siongui@gmail.com>
2017-04-18 06:44:39 +0800

    initial commit

---
+++ setup/setuppath.go
@@ -0,0 +1,9 @@
+package main
+
+// input
+const localeDir = "../common/locale"
+const poDomain = "messages"
+
+// output
+const WebsiteDir = "website"
+const indexHtmlPath = WebsiteDir + "/index.html"
