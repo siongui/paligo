5db69bec9c616c5d68edcb8eaccfb43774424316
siongui <siongui@gmail.com>
2018-10-28 03:16:38 +0800

    move url-related func to another file

--- gopherjs/pali.go
+++ gopherjs/pali.go
@@ -20,16 +20,6 @@ func isDev() bool {
 	return Window.Location().Hostname() == "localhost"
 }
 
-func HttpWordJsonPath(word string) string {
-	/*
-		if isDev() {
-			return "/json/" + word + ".json"
-		}
-	*/
-	return "https://siongui.github.io/xemaauj9k5qn34x88m4h/" + word + ".json"
-	//return "/xemaauj9k5qn34x88m4h/" + word + ".json"
-}
-
 func handleInputKeyUp(e Event) {
 	switch keycode := e.KeyCode(); keycode {
 	case 13:
---
+++ gopherjs/urlhandle.go
@@ -0,0 +1,11 @@
+package main
+
+func HttpWordJsonPath(word string) string {
+	/*
+		if isDev() {
+			return "/json/" + word + ".json"
+		}
+	*/
+	return "https://siongui.github.io/xemaauj9k5qn34x88m4h/" + word + ".json"
+	//return "/xemaauj9k5qn34x88m4h/" + word + ".json"
+}
