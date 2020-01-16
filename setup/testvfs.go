96d8f005f9e9200206ca38da539eddac82709194
siongui <tesiongui@gmail.com>
2020-01-17 04:41:27 +0800

    test Pali words VFS

--- setup/testvfs.go
+++ setup/testvfs.go
@@ -1,13 +1,38 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"path"
+
 	vfs "github.com/siongui/gopaliwordvfs"
 )
 
+const websiteDir = "../website"
+const wordJsonDir = websiteDir + "/json/"
+
 func main() {
-	b, err := vfs.ReadFile("sacca")
+	files, err := ioutil.ReadDir(wordJsonDir)
 	if err != nil {
 		panic(err)
 	}
-	println(string(b))
+
+	for i, file := range files {
+		bVfs, err := vfs.ReadFile(file.Name())
+		if err != nil {
+			panic(err)
+		}
+
+		bReal, err := ioutil.ReadFile(path.Join(wordJsonDir, file.Name()))
+		if err != nil {
+			panic(err)
+		}
+
+		if !bytes.Equal(bVfs, bReal) {
+			panic(file.Name())
+		}
+
+		fmt.Println(i, file.Name(), "ok")
+	}
 }
