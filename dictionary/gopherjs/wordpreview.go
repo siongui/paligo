08a6cb6a0be6e1757018c2ea4e00d5983e82d7e8
siongui <siongui@gmail.com>
2020-07-24 02:43:41 +0800

    show word preview next to suggest menu if the corresponding setting is checked

--- dictionary/gopherjs/wordpreview.go
+++ dictionary/gopherjs/wordpreview.go
@@ -5,23 +5,37 @@ import (
 	"html/template"
 	"net/http"
 
+	. "github.com/siongui/godom"
 	"github.com/siongui/gopalilib/lib"
 	sg "github.com/siongui/gopherjs-input-suggest"
 )
 
+type WordPreview struct {
+	Word              string
+	BookNameShortExps []lib.BookNameWordExp
+}
+
 var setmpl = `
-{{range $bnwe := .}}
-<article class="message">
-  <div class="message-header">
-    <p>{{$bnwe.BookName}}</p>
-  </div>
-  <div class="message-body">
-    {{$bnwe.Explanation}}
-  </div>
-</article>
+<span class="previewWordName">{{ .Word }}</span>
+{{range $bnwe := .BookNameShortExps}}
+<div class="shortDicExp">
+  <span>{{$bnwe.BookName}}</span>
+  <span>{{$bnwe.Explanation}}</span>
+</div>
 {{end}}`
 
-func showWordPreviewByTemplate(wi lib.BookIdWordExps) {
+var wordPreviewElm *Object
+
+func setWordPreviewUI(word, rawhtml string) {
+	wordPreviewElm.SetInnerHTML(rawhtml)
+	wordPreviewElm.ClassList().Remove("is-hidden")
+	Document.QuerySelector(".suggest").ClassList().Add("suggest-is-absolute")
+	w := Document.QuerySelector(".suggest").Get("offsetWidth").String() + "px"
+	println(w)
+	wordPreviewElm.Style().SetLeft(w)
+}
+
+func showWordPreviewByTemplate(word string, wi lib.BookIdWordExps) {
 	// bnwes: (Book-Name, Word-Explanation)s
 	idexps := lib.BookIdWordExps2IdExpsAccordingToSetting(wi, bookIdAndInfos, getSetting(), navigatorLanguages)
 	bnwes := lib.IdExps2BookNameWordExps(
@@ -29,15 +43,18 @@ func showWordPreviewByTemplate(wi lib.BookIdWordExps) {
 		bookIdAndInfos,
 	)
 	t1, _ := template.New("wordExplanationPreview").Parse(setmpl)
+	wp := WordPreview{word, bnwes}
 	// Google Search: go html template output string
 	// https://groups.google.com/forum/#!topic/golang-nuts/dSFHCV-e6Nw
 	var buf bytes.Buffer
-	t1.Execute(&buf, bnwes)
-	println(buf.String())
+	t1.Execute(&buf, wp)
+	rawhtml := buf.String()
+
+	setWordPreviewUI(word, rawhtml)
 }
 
-func httpGetWordJson2(w string) {
-	resp, err := http.Get(HttpWordJsonPath(w))
+func httpGetWordJson2(word string) {
+	resp, err := http.Get(HttpWordJsonPath(word))
 	if err != nil {
 		return
 	}
@@ -50,10 +67,11 @@ func httpGetWordJson2(w string) {
 		return
 	}
 
-	showWordPreviewByTemplate(wi)
+	showWordPreviewByTemplate(word, wi)
 }
 
 func setupWordPreview() {
+	wordPreviewElm = Document.QuerySelector(".suggestedWordPreview")
 	sg.OnHighlightSelectedWord(func(word string) {
 		//println(word)
 		if !getSetting().IsShowWordPreview {
