c0fbe48357d2496953d4df00318795bd5417a969
siongui <siongui@gmail.com>
2020-07-24 23:17:40 +0800

    add js i18n gettext feature

---
+++ tipitaka/gopherjs/pali.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	. "github.com/siongui/godom"
+	jsgettext "github.com/siongui/gopherjs-i18n"
+	"github.com/siongui/paliDataVFS"
+)
+
+func getFinalShowLocale() string {
+	var supportedLocales = []string{"en_US", "zh_TW", "vi_VN", "fr_FR"}
+	var navigatorLanguages = Window.Navigator().Languages()
+	// show language according to site url and NavigatorLanguages API
+	locale := Document.GetElementById("site-info").Dataset().Get("locale").String()
+	if locale == "" {
+		return jsgettext.DetermineLocaleByNavigatorLanguages(navigatorLanguages, supportedLocales)
+	}
+	return locale
+}
+
+func main() {
+	//println(getFinalShowLocale())
+	jsgettext.SetupTranslationMapping(paliDataVFS.GetPoJsonBlob())
+	jsgettext.Translate(getFinalShowLocale())
+}
