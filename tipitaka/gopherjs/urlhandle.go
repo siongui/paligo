0cce95f44e5a5ed8dd47d8bd41eef24b9e99349e
siongui <siongui@gmail.com>
2020-08-04 00:06:55 +0800

    add word json url

--- tipitaka/gopherjs/urlhandle.go
+++ tipitaka/gopherjs/urlhandle.go
@@ -1,9 +1,24 @@
 package main
 
+import (
+	. "github.com/siongui/godom"
+)
+
 func ActionXmlUrl(action string) string {
 	return "https://siongui.github.io/tipitaka-romn/" + action
 }
 
 func GetXslUrl() string {
 	return "https://siongui.github.io/tipitaka-romn/cscd/tipitaka-latn.xsl"
+}
+
+func HttpWordJsonPath(word string) string {
+	if isOffline() {
+		return "/json/" + word + ".json"
+	}
+	return "https://siongui.github.io/xemaauj9k5qn34x88m4h/" + word + ".json"
+}
+
+func isOffline() bool {
+	return Window.Location().Hostname() == "localhost" && Window.Location().Port() == "8080"
 }
