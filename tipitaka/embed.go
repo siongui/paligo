4ba783295df82ed8150358e266d9dab5d4022558
siongui <siongui@gmail.com>
2020-08-07 23:29:45 +0800

    remove un-needed code and data

--- tipitaka/embed.go
+++ tipitaka/embed.go
@@ -6,15 +6,12 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"os"
 	"path"
-	"strconv"
 
-	"github.com/siongui/go-succinct-data-structure-trie"
 	"github.com/siongui/goef"
-	"github.com/siongui/gopalilib/dicutil"
 	"github.com/siongui/gopalilib/tpkutil"
 	"github.com/siongui/gopalilib/util"
-	"github.com/siongui/gopaliwordvfs"
 )
 
 func SaveTipitakaTreeJson(xmldir, outputdir string) {
@@ -27,57 +24,19 @@ func SaveTipitakaTreeJson(xmldir, outputdir string) {
 	util.SaveJsonFile(t, path.Join(outputdir, "tpktoc.json"))
 }
 
-func SaveBookIdAndInfosJson(datarepodir, outputdir string) {
-	bookCsv := path.Join(datarepodir, "dictionary/dict-books.csv")
-	fmt.Println(bookCsv)
-	dicutil.ParseDictionayBookCSV(bookCsv, path.Join(outputdir, "BookIdAndInfos.json"))
-}
-
-func SaveTrieJson(words []string, outputdir string) {
-	// set alphabet of words
-	bits.SetAllowedCharacters("abcdeghijklmnoprstuvyāīūṁṃŋṇṅñṭḍḷ…'’° -")
-	// encode: build succinct trie
-	te := bits.Trie{}
-	te.Init()
-
-	for i, word := range words {
-		fmt.Println(i, word)
-		te.Insert(word)
-	}
-
-	// encode: trie encoding
-	teData := te.Encode()
-	//fmt.Println(teData)
-	err := ioutil.WriteFile(path.Join(outputdir, "trie-data.txt"), []byte(teData), 0644)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(te.GetNodeCount())
-	err = ioutil.WriteFile(path.Join(outputdir, "trie-node-count.txt"), []byte(strconv.Itoa(int(te.GetNodeCount()))), 0644)
-	if err != nil {
-		panic(err)
-	}
-
-	// encode: build cache for quick lookup
-	rd := bits.CreateRankDirectory(teData, te.GetNodeCount()*2+1, bits.L1, bits.L2)
-	//fmt.Println(rd.GetData())
-	err = ioutil.WriteFile(path.Join(outputdir, "trie-rank-directory-data.txt"), []byte(rd.GetData()), 0644)
-	if err != nil {
-		panic(err)
-	}
-}
-
 func main() {
 	xmlDir := flag.String("xmlDir", "", "Tipiṭaka XML dir")
-	dataDir := flag.String("dataDir", "", "website data dir")
-	dataRepoDir := flag.String("dataRepoDir", "", "dir of data repo which contains pali data")
 	flag.Parse()
 
-	SaveTipitakaTreeJson(*xmlDir, *dataDir)
-	SaveBookIdAndInfosJson(*dataRepoDir, *dataDir)
-	SaveTrieJson(gopaliwordvfs.MapKeys(), *dataDir)
+	dir, err := ioutil.TempDir("", "tocjson")
+	if err != nil {
+		panic(err)
+	}
+	defer os.RemoveAll(dir) // clean up
 
-	err := goef.GenerateGoPackagePlainText("main", *dataDir, "gopherjs/data.go")
+	SaveTipitakaTreeJson(*xmlDir, dir)
+
+	err = goef.GenerateGoPackagePlainText("main", dir, "gopherjs/data.go")
 	if err != nil {
 		panic(err)
 	}
