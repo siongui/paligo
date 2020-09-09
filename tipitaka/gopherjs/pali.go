3370517aacaf85990ac7782728493021576febc0
siongui <siongui@gmail.com>
2020-09-10 00:43:14 +0800

    move common frontend code to libfrontend

--- tipitaka/gopherjs/pali.go
+++ tipitaka/gopherjs/pali.go
@@ -5,7 +5,6 @@ import (
 
 	. "github.com/siongui/godom"
 	"github.com/siongui/gopalilib/lib"
-	"github.com/siongui/gopalilib/lib/jsgettext"
 	"github.com/siongui/gopalilib/libfrontend"
 	"github.com/siongui/gopalilib/libfrontend/everyword"
 	"github.com/siongui/gopalilib/libfrontend/setting"
@@ -13,17 +12,6 @@ import (
 	"github.com/siongui/gopalilib/libfrontend/velthuis"
 	"github.com/siongui/gopalilib/libfrontend/xslt"
 )
-
-func getFinalShowLocale() string {
-	var supportedLocales = []string{"en_US", "zh_TW", "vi_VN", "fr_FR"}
-	var navigatorLanguages = Window.Navigator().Languages()
-	// show language according to site url and NavigatorLanguages API
-	locale := Document.GetElementById("site-info").Dataset().Get("locale").String()
-	if locale == "" {
-		return jsgettext.DetermineLocaleByNavigatorLanguages(navigatorLanguages, supportedLocales)
-	}
-	return locale
-}
 
 func xmlAction(t lib.Tree) {
 	// FIXME: show loading not working on Chromium
@@ -48,17 +36,8 @@ func xmlAction(t lib.Tree) {
 	ToggleMobileTreeview()
 }
 
-func TranslateDocument(locale string) {
-	elms := Document.QuerySelectorAll("[data-default-string]")
-	for _, elm := range elms {
-		str := elm.Get("dataset").Get("defaultString").String()
-		elm.Set("textContent", jsgettext.Gettext(locale, str))
-	}
-}
-
 func main() {
-	//println(getFinalShowLocale())
-	TranslateDocument(getFinalShowLocale())
+	libfrontend.TranslateDocument(libfrontend.GetFinalShowLocale())
 
 	b, _ := ReadFile("tpktoc.json")
 	//println(string(b))
